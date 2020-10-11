5451ac6a4b3512a0e715fb6f42d87183811f3d68
kanginhan <hunt2433@naver.com>
2020-10-11 23:02:34 +0900

    Updates

---
+++ precache-manifest.bfe5076bca96a3d02db7a9c971b8b99d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb0cfb744a921a5bdffc85309faf0d44",
+    "url": "/uplussave_apply/index.html"
+  },
+  {
+    "revision": "543d99a0f5b841f57aa5",
+    "url": "/uplussave_apply/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "4e2ca87cadbbde6c96f5",
+    "url": "/uplussave_apply/static/js/2.53a9826a.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/uplussave_apply/static/js/2.53a9826a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "543d99a0f5b841f57aa5",
+    "url": "/uplussave_apply/static/js/main.422e49bc.chunk.js"
+  },
+  {
+    "revision": "72a09223044625a4f728",
+    "url": "/uplussave_apply/static/js/runtime-main.9686d23e.js"
+  },
+  {
+    "revision": "603c8e751580391907d1397299371fb5",
+    "url": "/uplussave_apply/static/media/idcard_correct.603c8e75.png"
+  },
+  {
+    "revision": "aaf1af9e4eeb297967e0e53c2bd6fcfd",
+    "url": "/uplussave_apply/static/media/idcard_wrong.aaf1af9e.png"
+  }
+]);
